6481c05412cfd2ca34f2c1409030367a27af6cda
soy cristian <cristiansandoval2202@gmail.com>
2024-05-01 03:42:59 -0500

    portafolio web en proceso

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let boton = document.getElementById("icon")
+let navBarMenu =  document.getElementById("links")
+
+boton.addEventListener('click', () => {
+    navBarMenu.classList.add('uno');
+});
+
+
